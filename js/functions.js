c61b8f1c2462c0ee2a7446de4bac29140ea2e54b
unknown <ransome@IT4.lapf2000.co.zw>
2014-06-03 18:51:17 +0200

    navigational menu js and extra styling css

--- js/functions.js
+++ js/functions.js
@@ -5,5 +5,41 @@
  */
 ( function( $ ) {
 
+	function showItem(item)
+	{
+		//change display from none to block
+		$(item).show();
+	}
+
+
+	function hideItem(item)
+	{
+				//change display from block to none
+
+		$(item).hide();
+	}
+
+
+	$("ul li").hover(function(){
+
+
+
+			if($(this).children(".sub-menu").length>0)
+			{
+				var submenu = $(this).children(".sub-menu")[0];
+				showItem(submenu);
+			}
+
+
+	},function(){
+
+		if($(this).children(".sub-menu").length>0)
+			{
+				var submenu = $(this).children(".sub-menu")[0];
+				hideItem(submenu);
+			}
+
+	});
+
 
 } )( jQuery );
