7963c55090c6278154fc4386de1b581d5b7f22cd
unknown <ransome@IT4.lapf2000.co.zw>
2014-06-04 15:25:05 +0200

    Styling The default sidebar

--- sidebar.php
+++ sidebar.php
@@ -4,19 +4,6 @@
  */
 ?>
 <div id="secondary">
-	<?php
-		$description = get_bloginfo( 'description', 'display' );
-		if ( ! empty ( $description ) ) :
-	?>
-	<h2 ><?php echo esc_html( $description ); ?></h2>
-	<?php endif; ?>
-
-	<?php if ( has_nav_menu( 'secondary' ) ) : ?>
-	<nav role="navigation" >
-		<?php wp_nav_menu( array( 'theme_location' => 'secondary' ) ); ?>
-	</nav>
-	<?php endif; ?>
-
 	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 	<div id="primary-sidebar" role="complementary">
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
